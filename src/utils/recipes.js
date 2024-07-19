41218ede5fcc49758f49916421ddf25fb90c4b51
AnkurDutta21 <emxax54@gmail.com>
2024-07-19 13:20:54 +0530

    bug fix

--- src/utils/recipes.js
+++ src/utils/recipes.js
@@ -1,56 +1,64 @@
+import image1 from '../assets/image (2).jpg'
+import image2 from '../assets/image (3).jpg'
+import image3 from '../assets/image (4).jpg'
+import image4 from '../assets/image (5).jpg'
+import image5 from '../assets/image (6).jpg'
+import image6 from '../assets/image (7).jpg'
+import image7 from '../assets/image (8).jpg'
+import image8 from '../assets/image (9).jpg'
 const recipes = [
     {
-        name: 'Bacon Burger',
-        imgUrl: 'https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80',
-        author: 'Gordon Ramsay',
+        name: 'Pad Thai',
+        imgUrl: image1,
+        author: 'Justin Pichetrungsi',
         rating: 4.0,
         ratingsNumber:85
     },
     {
-        name: 'Shrimp Jumbalaya',
-        imgUrl: 'https://images.unsplash.com/photo-1515443961218-a51367888e4b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-        author: 'Jim Smith',
+        name: 'Cheese Fries',
+        imgUrl: image2, 
+        author: ' Thomas Keller',
         rating: 3.5,
         ratingsNumber:24
     },
     {
-        name: 'Magherita Pizza',
-        imgUrl: 'https://images.unsplash.com/photo-1528137871618-79d2761e3fd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        author: 'Chef Romano',
+        name: 'Mashed Potatoes',
+        imgUrl: image3,
+        author: 'Anthony Bourdain',
         rating: 4.3,
         ratingsNumber:103
     },
     {
         name: 'Classic Cubano',
-        imgUrl: 'https://images.unsplash.com/photo-1481070414801-51fd732d7184?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1225&q=80',
+        imgUrl: image4,
         author: 'Jon Favreau',
         rating: 5.0,
         ratingsNumber:149
     },
     {
         name: 'Spinach Pasta',
-        imgUrl: 'https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-        author: 'Gary Denowitz',
+        imgUrl: image5,
+        author: 'Paula Wolfert',
         rating: 2.4,
         ratingsNumber:12
     },
     {
         name: 'Blueberry Pie',
-        imgUrl: 'https://images.unsplash.com/photo-1459663561442-984db4f334e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        imgUrl: image6,
         author: 'Martha Stewart',
         rating: 4.3,
         ratingsNumber:102
     },
     {
-        name: 'Craft Burger',
-        imgUrl: 'https://images.unsplash.com/photo-1549611016-3a70d82b5040?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1316&q=80',
+        name: 'Veggie Delight',
+        imgUrl: image7,
         author: 'Robbie Carroll',
         rating: 3.5,
         ratingsNumber:122
     },
     {
-        name: 'Broiled Salmon',
-        imgUrl: 'https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+        name: 'Broiled KhusKhus',
+        imgUrl: image8,
         author: 'Chef Romano',
         rating: 3.5,
         ratingsNumber:42
